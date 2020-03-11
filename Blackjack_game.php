6d71af6c1ee8c213002ae3527f9fd16bad595766
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-03-11 16:27:49 +0000

    totaling scores

--- Blackjack_game.php
+++ Blackjack_game.php
@@ -58,36 +58,82 @@ $deck = [
         'King'=> 10,
         'Ace'=> 11],
 ];
-function draw_card($deck) {
+
+//Calculating Player 1 score
+function draw_card1($deck) {
     $random_suit = array_rand($deck);
     $random_card = array_rand($deck[$random_suit]);
-    $selected_card = [
+    $selected_card1 = [
         'name' => $random_card . ' of ' . $random_suit,
         'score' => $deck[$random_suit][$random_card],
     ];
     unset($deck[$random_suit][$random_card]);
-    return $selected_card;
+    return $selected_card1;
 };
 
+$selected_card1 = draw_card1($deck);
+echo $selected_card1['score'] . '<br>';
 
-$card_drawn = draw_card($deck);
+function draw_card2($deck) {
+    $random_suit = array_rand($deck);
+    $random_card = array_rand($deck[$random_suit]);
+    $selected_card2 = [
+        'name' => $random_card . ' of ' . $random_suit,
+        'score' => $deck[$random_suit][$random_card],
+    ];
+    unset($deck[$random_suit][$random_card]);
+    return $selected_card2;
+};
 
-$player1_score =
+$selected_card2 = draw_card2($deck);
+echo $selected_card2['score'] . '<br>';
+
+//Player one total score
+$player1_score = $selected_card1['score'] + $selected_card2['score'];
+echo 'Total score for Player 1: ' .  $player1_score . '<br>';
 
 
+//Calculating Player 2 score
+function draw_card3($deck) {
+    $random_suit = array_rand($deck);
+    $random_card = array_rand($deck[$random_suit]);
+    $selected_card3 = [
+        'name' => $random_card . ' of ' . $random_suit,
+        'score' => $deck[$random_suit][$random_card],
+    ];
+    unset($deck[$random_suit][$random_card]);
+    return $selected_card3;
+};
+
+$selected_card3 = draw_card3($deck);
+echo $selected_card3['score'] . '<br>';
+
+function draw_card4($deck) {
+    $random_suit = array_rand($deck);
+    $random_card = array_rand($deck[$random_suit]);
+    $selected_card4 = [
+        'name' => $random_card . ' of ' . $random_suit,
+        'score' => $deck[$random_suit][$random_card],
+    ];
+    unset($deck[$random_suit][$random_card]);
+    return $selected_card4;
+};
+
+$selected_card4 = draw_card4($deck);
+echo $selected_card4['score'] . '<br>';
+
+$player2_score = $selected_card3['score'] + $selected_card4['score'];
+echo 'Total score for Player 2: ' .  $player2_score;
 
 
-//$player1_score =
-//
-//
-//if ($player1_score >= $player2_score && <= 21) {
-//    echo 'Player 1 wins';
-//} else if ($player2_score >= $player1_score && <= 21) {
-//    echo 'Player 2 wins';
-//} else if ($player1_score == $player2_score && <= 21) {
-//    echo 'You draw';
-//    }
-//};
+if ($player1_score >= $player2_score && $player1_score < 22) {
+    echo 'Player 1 wins';
+} else if ($player2_score >= $player1_score && $player1_score < 22) {
+    echo 'Player 2 wins';
+} else if ($player1_score == $player2_score && <= 21) {
+    echo 'You draw';
+    }
+};
 
 
 //echo '<pre>';
