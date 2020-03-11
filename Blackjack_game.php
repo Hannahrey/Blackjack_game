cb1a44eef15754cf6658d6f35d0b143ec406db26
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-03-11 09:54:10 +0000

    Working on an else if statement

--- Blackjack_game.php
+++ Blackjack_game.php
@@ -59,9 +59,24 @@ $card_deck = [
         'Ace'=> 11],
 ];
 
-echo '<pre>';
-var_dump($card_deck);
-echo '</pre>';
+$random_deck= array_rand($card_deck, 2);
+echo $random_deck();
 
-return shuffle($card_deck);
+
+
+if ($player1_score >= $player2_score && <= 21) {
+    echo 'Player 1 wins';
+} else if ($player2_score >= $player1_score && <= 21) {
+    echo 'Player 2 wins';
+} else if ($player1_score == $player2_score && <= 21) {
+    echo 'You draw';
+    }
+};
+
+
+//echo '<pre>';
+//var_dump($card_deck);
+//echo '</pre>';
+//
+//return shuffle($card_deck);
 
